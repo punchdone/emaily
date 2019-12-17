7e75cf89fb6f552802fd77160062405a7474bc5a
sreader <sreader@me.com>
2019-12-17 11:16:16 -0800

    finished auth flow

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+require('./models/user');
+require('./services/passport');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-//clientId 414082177470-hp5ce3eccc11i9usdvcpsvsgp4rh8mh7.apps.googleusercontent.com
-//clientSecret 4WJTQevlf2yheJFai3tg46SB
-passport.use(new GoogleStrategy());
-//http://localhost:5000/auth/google/callback
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+require('./routes/authRoutes')(app);
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => 'server is running on ${PORT}');
